92b0e8b4c74dc3b10750e1c10e979f9fa7e7ac58
Alex <alexcocos46@gmail.com>
2021-10-27 11:38:06 +0300

    laborator3

---
+++ DoughnutMachineBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Cocos_Alexandru_Lab2
+{
+    internal class DoughnutMachineBase
+    {
+        DispatcherTimer doughnutTimer;
+        private void InitializeComponent()
+        {
+            this.doughnutTimer = new DispatcherTimer();
+            this.doughnutTimer.Tick += new System.EventHandler(this.doughnutTimer_Tick);
+        }
+    }
+}
